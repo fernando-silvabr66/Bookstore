2930c49562f424e99fc54e080eeba3fd21da390d
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-04-02 17:26:21 -0300

    update configstore.js file

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
+import statusReducer from './categories/categories';
 
 const configReducer = combineReducers({
-  booksReducer,
+  booksReducer, statusReducer,
 });
 
 const store = createStore(configReducer, applyMiddleware(logger));
