2b0257fd1381687824d752eab1789f8037a502c2
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-04-09 10:26:11 -0300

    Update Header.jsx style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/Bookstore/books">Books</Link>
-        </li>
-        <li>
-          <Link to="/Bookstore/categories">Categories</Link>
-        </li>
-      </ul>
+  <header className="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-light bg-white p-5">
+      <div className="container">
+        <h1 className="display-4">BookStore CMS</h1>
+
+        <div className="navbar-collapse">
+          <ul className="navbar-nav m-3">
+            <li className="nav-item active">
+              <Link to="/Bookstore/books" className="nav-link">
+                Books
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Bookstore/categories" className="nav-link">Categories</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <i className="fa fa-user"> </i>
+        </div>
+      </div>
     </nav>
   </header>
 );
